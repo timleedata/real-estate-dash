6a1b98ba86e8ccb18ca9be3d7b2c8db2f639f104
timleedata <timleedata@gmail.com>
2021-02-13 22:23:59 -0500

    add pd and read csv for graph

--- atlmkt_dash/plotly_test.py
+++ atlmkt_dash/plotly_test.py
@@ -1,37 +1,53 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
+import plotly.graph_objs as go
+import numpy as np
+import pandas as pd
 from django_plotly_dash import DjangoDash
 
-app = DjangoDash('SimpleExample')
+app = DjangoDash('AtlMktDash')
+
+df = pd.read_csv('static/data/rs_df.csv')
+neighborhood_list = np.insert(arr=df['neighborhood'].unique(), obj=0, values='None selected', axis=0)
 
 app.layout = html.Div([
-    dcc.RadioItems(
-        id='dropdown-color',
-        options=[{'label': c, 'value': c.lower()} for c in ['Red', 'Green', 'Blue']],
-        value='red'
+    dcc.Dropdown(
+        id='neighborhood-name',
+        options=[{'label': name, 'value': name.lower()} for name in neighborhood_list],
+        value='none selected'
     ),
-    html.Div(id='output-color'),
-    dcc.RadioItems(
-        id='dropdown-size',
-        options=[{'label': i, 'value': j} for i, j in [('L','large'), ('M','medium'), ('S','small')]],
-        value='medium'
-    ),
-    html.Div(id='output-size')
-
+    html.Div(id='dash-output')
 ])
 
 @app.callback(
-    dash.dependencies.Output('output-color', 'children'),
-    [dash.dependencies.Input('dropdown-color', 'value')])
-def callback_color(dropdown_value):
-    return "The selected color is %s." % dropdown_value
-
-@app.callback(
-    dash.dependencies.Output('output-size', 'children'),
-    [dash.dependencies.Input('dropdown-color', 'value'),
-     dash.dependencies.Input('dropdown-size', 'value')])
-def callback_size(dropdown_color, dropdown_size):
-    return "The chosen T-shirt is a %s %s one." %(dropdown_size,
-                                                  dropdown_color)
+    dash.dependencies.Output('dash-output', 'children'),
+    [dash.dependencies.Input('neighborhood-name', 'value')])
+def callback_name(neighborhood_name):
+    if neighborhood_name != "none selected":
+        N = 500
+        random_x = np.linspace(0, 1, N)
+        random_y = np.random.randn(N)
+    
+        # Create a trace
+        trace = go.Scatter(x=random_x,
+                           y=random_y)
+    
+        data = [trace]
+    
+        layout = dict(title='',
+                      yaxis=dict(zeroline=False, title='Average Value ($)',),
+                      xaxis=dict(zeroline=False, title='Date', tickangle=0),
+                      margin=dict(t=20, b=50, l=50, r=40),
+                      height=350,
+                     )
+    
+    
+        fig = dict(data=data, layout=layout)
+        line_graph = dcc.Graph(id='line-area-graph2', figure=fig, style={'display':'inline-block', 'width':'100%',
+                                                                         'height':'100%;'})
+        children = [line_graph]
+    
+        return children
+    else:
+        return "None selected\<br\>Pandas, numpy version: %s %s" %(pd.__version__, np.__version__)
--- tldata_web/settings.py
+++ tldata_web/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_plotly_dash.middleware.BaseMiddleware',
 ]
 
 ROOT_URLCONF = 'tldata_web.urls'
